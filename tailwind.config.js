22542614f3b32fc6ba1e74a9a118ff877ee5bd47
Naziur-Rahman <naziurr85@gmail.com>
2024-10-17 02:55:53 +0600

    custome color & padding setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,25 @@ export default {
         Dots: ['Edu AU VIC WA NT Dots', 'cursive'],
         FatFace: ['Abril Fatface', 'serif'],
       },
+      colors:{
+        primary: '#f19509',
+        primaryDark: "#e86f00",
+        lightOrange: "#f1dabf",
+        darkGray: "#1a1f25",
+        lightGray: "#272c35",
+        bgColor: 'bg-gradient-to-r from-cyan-500 to-blue-500',
+        danger: '#e74c3c',
+        success: '#27ae60',
+        
+        
+      },
+      container:{
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          
+      }
     },
   },
   plugins: [
